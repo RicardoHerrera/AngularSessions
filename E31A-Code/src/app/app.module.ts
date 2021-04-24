23f0c4873b0b1c6d5928230429ddf43e27b83e9a
Ricardo Herrera <aherreric@gmail.com>
2021-04-24 13:58:18 -0500

    minor changes

--- E31A-Code/src/app/app.module.ts
+++ E31A-Code/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ApartmentComponent } from './pages/apartment/apartment.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { UserComponent } from './private/user/user.component';
 import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { LoginComponent } from './login/login.component';
     ProjectComponent,
     ApartmentComponent,
     UserComponent,
-    LoginComponent
+    LoginComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
